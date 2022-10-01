2d1a2b61c902c69c58aa5b6c3aeaa956552e5185
capriciousbased <mdanisulhoquekhan@gmail.com>
2022-10-01 12:03:16 +0200

    Home Page added

---
+++ src/components/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-700 lg:text-2xl md:text-xl sm:text-3xl py-2">
+          Hi, my name is
+        </p>
+        <h1 className="text-4xl sm:text-6xl font-bold text-[#ccd6f6]">
+          Anisul Hoque Khan
+        </h1>
+        <h2 className="text-4xl sm:text-6xl font-bold text-[#8892b0] py-4">
+          I'm a Frontend Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px] text-2xl sm:text-xl">
+          I’m specializing in building (and occasionally designing) exceptional
+          digital experiences. Currently, I’m focused on building responsive web
+          applications.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3 " />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
